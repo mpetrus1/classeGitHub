13830c9d59a2a1d9a3767e0780b53671235bee90
Miquel <mpec001@gmail.com>
2025-10-06 18:07:19 +0200

    4a linia comentaris

--- exemple.java
+++ exemple.java
@@ -2,4 +2,4 @@
 //Modificacio feta des de la segona branca
 //Modificacio del fitxer des de la branca master
 //Afegida una tercera linia de modificacio des de modificacio
-
+//4a linia de modificacio
